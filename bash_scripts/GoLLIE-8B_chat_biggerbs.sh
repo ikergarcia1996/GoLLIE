57007bea4377718986c2a9f8a702fe481bd20daa
OSainz <osainz59@gmail.com>
2024-09-17 12:03:25 +0200

    Development scripts

--- bash_scripts/GoLLIE-8B_chat_biggerbs.sh
+++ bash_scripts/GoLLIE-8B_chat_biggerbs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=GoLLIE-8B-chat
 #SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:1
-#SBATCH --mem=128G
-#SBATCH --output=.slurm/GoLLIE-8B-chat.out
-#SBATCH --error=.slurm/GoLLIE-8B-chat.err
+#SBATCH --gres=gpu:2
+#SBATCH --mem=256G
+#SBATCH --output=.slurm/GoLLIE-8B-chat_bsbigger.out
+#SBATCH --error=.slurm/GoLLIE-8B-chat_bsbigger.err
 
 source /ikerlariak/osainz006/venvs/collie/bin/activate
 
@@ -20,7 +20,6 @@ export OMP_NUM_THREADS=16
 echo CUDA_VISIBLE_DEVICES "${CUDA_VISIBLE_DEVICES}"
 
 export PYTHONPATH="$PYTHONPATH:$PWD"
-torchrun --standalone --master_port 37227 --nproc_per_node=1 src/run.py configs/model_configs/GoLLIE-Llama-3.1-8B-chat.yaml
-# python src/run.py configs/model_configs/eval/GoLLIE-7B_Mistral_BS128_R128.yaml
+torchrun --standalone --master_port 37227 --nproc_per_node=2 src/run.py configs/model_configs/GoLLIE-Llama-3.1-8B-chat_2gpus.yaml
 
 
